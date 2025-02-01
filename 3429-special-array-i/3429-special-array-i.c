815006c3da2f787bb262e275806aa8fb5d9fa3b2
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2025-02-01 09:43:20 +0530

    Time: 0 ms (100%), Space: 8.6 MB (53.82%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.c
@@ -0,0 +1,10 @@
+bool isArraySpecial(int* nums, int numsSize) {
+ for(int i=0;i<numsSize-1;i++)
+ {
+    if((nums[i+1]%2==0 && nums[i]%2==0) || (nums[i]%2!=0 && nums[i+1]%2!=0))
+    {
+        return false;
+    }
+ }
+ return true;
+}
