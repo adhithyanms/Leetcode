1838763f7e94834e3525b83278e20b52e48cc067
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2025-02-17 11:34:24 +0530

    Time: 0 ms (100%), Space: 9.1 MB (51.65%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.c
@@ -0,0 +1,12 @@
+int maxProfit(int* prices, int pricesSize) 
+{
+    int profit;
+    for(int i=0;i<pricesSize-1;i++)
+    {
+       if(prices[i]<prices[i+1] )
+       {
+        profit+=prices[i+1]-prices[i];
+       }
+    }
+    return profit;
+}
