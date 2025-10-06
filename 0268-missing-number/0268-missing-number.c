dc0957f64180fa809dc268f10a1737b9fb110a9b
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2025-10-06 20:53:48 +0530

    Time: 9 ms (11.84%), Space: 6.2 MB (100%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.c
@@ -0,0 +1,10 @@
+int missingNumber(int* nums, int numsSize) 
+{   
+    int count=0;
+    for(int i=0;i<numsSize;i++)
+    {
+       count+=nums[i];
+    }
+    return (numsSize*(numsSize+1)/2)-count;
+    
+}
