c5101c45a4c9fd87bce1f5a795ed6ea3339512b9
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-02-05 16:23:20 +0530

    Time: 1 ms (99.68%), Space: 45.4 MB (60.66%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int compress(char[] chars) {
+        int index = 0;
+        int count = 1;
+
+        for(int i = 1; i <= chars.length; i++){
+            if(i < chars.length && chars[i] == chars[i-1]){
+                count++;
+            }
+            else{
+                chars[index++] = chars[i-1];
+
+                if(count > 1){
+                    String s = Integer.toString(count);
+                    for(char c : s.toCharArray()){
+                        chars[index++] = c;
+                    }
+                }
+                count = 1;
+            }
+        }
+        return index;
+    }
+}
