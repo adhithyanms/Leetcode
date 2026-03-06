a6942838979f79383afb38379b4146731f897ce2
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-03-06 13:54:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,2 +1,5 @@
-select actor_id ,director_id from ActorDirector group  by actor_id ,director_id 
- Having count(timestamp)>=3
+select actor_id , director_id
+From ActorDirector 
+-- where actor_id = director_id
+group by actor_id 
+having count(*)>=3 ;
