290d1fb51f1bee9fbcf5f3d2a7336d56bacb79c2
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-03-06 10:09:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,5 +1,8 @@
-select s.name from SalesPerson s
-except select  p.name from SalesPerson p 
-left join orders o on p.sales_id=o.sales_id 
-left join company c on c.com_id=o.com_id 
-where c.name='RED'
+select name from SalesPerson 
+where sales_id not in(
+    select o.sales_id 
+    from Orders o
+    Join Company c 
+    on o.com_id = c.com_id
+    where c.name = 'RED'
+)
