ee4843d059ac7e8dd1893ad226af44ada35f70ec
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-03-07 11:56:16 +0530

    Time: 790 ms (12.79%), Space: 0B (100%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,8 @@
+SELECT E1.employee_id , E1.name , 
+COUNT(E2.reports_to) AS reports_count ,
+ROUND(AVG(E2.age)) AS average_age 
+FROM Employees E1 
+INNER JOIN Employees E2
+ON E1.employee_id = E2.reports_to 
+GROUP BY E1.employee_id , E1.name
+ORDER BY E1.employee_id;
