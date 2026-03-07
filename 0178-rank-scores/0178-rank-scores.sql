a100de4a37f8cfc3729dfbc4c6e242f167fba5ef
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-03-07 14:31:20 +0530

    Time: 826 ms (6.21%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,9 @@
+SELECT 
+    S1.score,
+    1 + (
+        SELECT COUNT(DISTINCT S2.score)
+        FROM Scores S2
+        WHERE S2.score > S1.score
+    ) AS 'rank'
+FROM Scores S1
+ORDER BY score DESC;
