52afaaf5d38d1e4747c68c6f0cd7ede5ba62efab
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-03-07 09:53:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,3 +1,7 @@
 SELECT * 
 FROM Users 
-WHERE mail REGEXP '^[a-z][-._a-z0-9]*@leetcode[.]com$'
+WHERE mail REGEXP '^[A-Za-z][-._A_Za-z0-9]*@leetcode[.]com$'
+
+-- SELECT *
+-- FROM Users
+-- WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$';
