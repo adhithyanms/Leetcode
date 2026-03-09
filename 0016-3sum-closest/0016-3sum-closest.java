8c57a8ea03d7204612d99d39786c1b3eccd47309
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-03-09 11:02:01 +0530

    Time: 16 ms (92.08%), Space: 45.7 MB (46.39%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,25 +1,28 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-       int colestSum=nums[0]+nums[1]+nums[2];
-       int minDiffer=Integer.MIN_VALUE;
-       Arrays.sort(nums);
-       for(int i=0;i<nums.length-2;i++){
-        int left=i+1;
-        int right=nums.length-1;
-        while(left<right){
-            int sum=nums[i]+nums[left]+nums[right];
-            minDiffer=Math.min(minDiffer,Math.abs(sum-target));
-            if(sum==target){
-                return sum;
-            }
-            if(sum>target){
-                right--;
-            }
-            else{
-               left++;
+        int minDiffer = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        int closestSum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                int diff = Math.abs(sum - target);
+                if (diff < minDiffer) {
+                    minDiffer = diff;
+                    closestSum = sum;
+                }
+                if (sum == target) {
+                    return sum;
+                }
+                if (sum > target) {
+                    right--;
+                } else {
+                    left++;
+                }
             }
         }
-       }
-       return colestSum;
+        return closestSum;
     }
 }
