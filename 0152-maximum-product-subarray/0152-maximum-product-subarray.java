b474795c21dae886168cff95009067efa833ec96
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-03-14 10:20:07 +0530

    Time: 1 ms (90.47%), Space: 46.6 MB (99.22%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int ans = nums[0];
+        int left = 1, right = 1;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            left = left == 0 ? 1 : left;
+            right = right == 0 ? 1 : right;
+            left *= nums[i];
+            right *= nums[n - 1 - i];
+            ans = Math.max(ans, Math.max(left, right));
+        }
+        return ans;
+    }
+}
