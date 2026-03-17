f4f3241e41ad13391406d267b53982be90d057a3
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-03-17 21:22:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.java
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
+        int maxWidth = 0;
         Stack<Integer> stack = new Stack<>();
-        for(int i = 0 ; i<nums.length ; i++ ){
+        for(int i = 0 ; i<nums.length ;i++){
             if(stack.isEmpty() || nums[stack.peek()]>nums[i]){
                 stack.push(i);
             }
+            // stack.push(i);
         }
-        int maxWidth = 0;
-        for(int i = nums.length-1 ;i>=0 ;i--){
-            while(!stack.isEmpty() && nums[stack.peek()] <=nums[i]){
-                maxWidth = Math.max(i - stack.peek(),maxWidth);
+        for(int i = nums.length-1;i>=0;i--){
+            while(!stack.isEmpty() && nums[stack.peek()]<=nums[i]){
+                maxWidth = Math.max(i-stack.peek() , maxWidth);
                 stack.pop();
             }
         }
