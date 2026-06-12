a78b6ff25a5e45df84fb0814efb43f7dafd8d14d
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-06-12 21:21:10 +0530

    Time: 0 ms (100%), Space: 42.8 MB (55.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,30 @@
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int dp[] = new int[n+1];
+//         if(n ==1 ){
+//             return nums[0];
+//         }
+//         dp[0] = nums[0];
+//         dp[1] = Math.max(nums[0], nums[1]);
+//         for (int i = 2; i < n; i++) {
+//             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+//         }
+//         return dp[n-1];
+//     }
+// }
+
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n+1];
-        if(n ==1 ){
-            return nums[0];
+        int prevRob = 0;
+        int maxRob = 0;
+
+        for (int curValue : nums) {
+            int temp = Math.max(maxRob, prevRob + curValue);
+            prevRob = maxRob;
+            maxRob = temp;
         }
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-        return dp[n-1];
+
+        return maxRob;        
     }
 }
