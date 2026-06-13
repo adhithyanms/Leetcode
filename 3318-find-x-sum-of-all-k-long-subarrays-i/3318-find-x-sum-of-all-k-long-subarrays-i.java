d706bf244aa3cc23a63c3120d1588b3783f5f5d3
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-06-13 08:59:01 +0530

    Time: 6 ms (94.83%), Space: 47 MB (82.41%) - LeetHub

--- 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.java
+++ 3318-find-x-sum-of-all-k-long-subarrays-i/3318-find-x-sum-of-all-k-long-subarrays-i.java
@@ -1,41 +1,35 @@
-import java.util.*;
-
 class Solution {
     public int[] findXSum(int[] nums, int k, int x) {
-        int[] freq = new int[51];  
+        int freq[] = new int[51];
         int n = nums.length;
-        int[] ans = new int[n - k + 1];
-
+        int ans[] = new int[n - k + 1];
         for (int i = 0; i <= n - k; i++) {
             Arrays.fill(freq, 0);
-
             for (int j = i; j < i + k; j++) {
                 freq[nums[j]]++;
             }
             List<int[]> freqList = new ArrayList<>();
-            for (int value = 0; value < 51; value++) {
-                if (freq[value] > 0) {
-                    freqList.add(new int[]{value, freq[value]});
+            for (int v = 0; v < 51; v++) {
+                if (freq[v] > 0) {
+                    freqList.add(new int[] { v, freq[v] });
                 }
             }
             freqList.sort((a, b) -> {
-                if (b[1] == a[1]) {     
+                if (a[1] == b[1]) {
                     return b[0] - a[0];
                 }
-                return b[1] - a[1];      
+                return b[1] - a[1];
             });
-
             int xSum = 0;
             int count = 0;
-            for (int[] pair : freqList) {
-                if (count == x) break;
-                xSum += pair[0] * pair[1];  
+            for (int pair[] : freqList) {
+                if (count == x)
+                    break;
                 count++;
+                xSum += pair[0] * pair[1];
             }
-
             ans[i] = xSum;
         }
-
         return ans;
     }
 }
