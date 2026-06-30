d1667aad96cfba67daac07755626cf01d0c7da11
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-06-30 21:35:52 +0530

    Time: 1540 ms (11.4%), Space: 46.5 MB (40.19%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,23 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
-        StringBuilder ans =new StringBuilder();
+        StringBuilder ans = new StringBuilder();
+        int n = s.length();
         ans.append(s.charAt(0));
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j < s.length(); j++) {
-                if (isPolider(s, i, j)) {
-                    if(j-i+1 >ans.length()){
-                     ans=new StringBuilder(s.substring(i,j+1));
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                if(isPalindrom(s,i,j)){
+                    if(j-i+1>ans.length()){
+                        ans = new StringBuilder(s.substring(i,j+1));
                     }
                 }
             }
         }
         return ans.toString();
     }
-
-    public boolean isPolider(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start) != s.charAt(end))
+    public boolean isPalindrom(String s , int start , int end){
+        while(start<end){
+            if(s.charAt(start)!=s.charAt(end)){
                 return false;
+            }
             start++;
             end--;
         }
