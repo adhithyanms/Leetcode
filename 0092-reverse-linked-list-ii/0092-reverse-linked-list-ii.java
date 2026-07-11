0ecc2ea8c1cd6b65fa51873fd680259d3a5b14cc
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-07-11 14:46:30 +0530

    Time: 0 ms (100%), Space: 43.3 MB (12.36%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,31 +10,30 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if (head == null || head.next == null || left == right)
+        if(head == null || head.next == null || left == right){
             return head;
+        }
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode pre = dummy;
-        for (int i = 1; i < left; i++)
-            pre = pre.next;
-        ListNode start = pre.next;
-        ListNode ans = reverse(start, right - left + 1);
-        pre.next = ans;
+        ListNode prev = dummy;
+        for(int i = 1;i<left;i++){
+            prev = prev.next;
+        }
+        ListNode start = prev.next;
+        ListNode ans = reverse(start , right-left+1);
+        prev.next = ans;
         return dummy.next;
     }
-
-    public ListNode reverse(ListNode head, int index) {
-        if (head == null || head.next == null)
-            return head;
-        ListNode current = head, pre = null;
-        while ((index-- > 0) && current != null) {
-            ListNode newNode = current.next;
-            current.next = pre;
-            pre = current;
-            current = newNode;
+    public static ListNode reverse(ListNode head , int index){
+        if(head == null || head.next == null)return head;
+        ListNode current = head,prev = null;
+        while((index-->0) && current!=null){
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
         }
         head.next = current;
-        return pre;
+        return prev;
     }
-
 }
