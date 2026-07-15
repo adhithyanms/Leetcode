78e9515d37b8d25fa8e6d16086260c352a6258a8
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-07-15 15:56:46 +0530

    Time: 16 ms (90.51%), Space: 120.5 MB (27.67%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.java
+++ 1499-max-value-of-equation/1499-max-value-of-equation.java
@@ -1,25 +1,27 @@
-class Pair{
+class Pair {
     int val;
     int row;
-    Pair(int val , int row){
-        this.val =val;
+
+    Pair(int val, int row) {
+        this.val = val;
         this.row = row;
     }
 }
+
 class Solution {
     public int findMaxValueOfEquation(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->b.val - a.val);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.val - a.val);
         int max = Integer.MIN_VALUE;
-        for(int point[] : points){
+        for (int point[] : points) {
             int rowi = point[0];
             int coli = point[1];
-            while(!pq.isEmpty() && rowi-pq.peek().row>k){
+            while (!pq.isEmpty() && rowi - pq.peek().row > k) {
                 pq.poll();
             }
-            if(!pq.isEmpty()){
-                max = Math.max(max , pq.peek().val+rowi+coli);
+            if (!pq.isEmpty()) {
+                max = Math.max(max, pq.peek().val + rowi + coli);
             }
-            pq.offer(new Pair(coli-rowi ,rowi ));
+            pq.offer(new Pair(coli - rowi, rowi));
         }
         return max;
     }
