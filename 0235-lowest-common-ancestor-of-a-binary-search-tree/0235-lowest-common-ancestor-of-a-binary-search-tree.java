4318abf0bd25ad00f0d4bb6651b3d0cd9248d481
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-07-17 09:54:42 +0530

    Time: 6 ms (97.11%), Space: 47.7 MB (55.11%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -15,7 +15,7 @@ class Solution {
             if(p.val<root.val && q.val<root.val){
                 root = root.left;
             }
-            if(p.val>root.val && q.val>root.val){
+            else if(p.val>root.val && q.val>root.val){
                 root = root.right;
             }
             else{
