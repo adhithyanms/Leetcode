ac801c9125bafb3135c304053849d6b0d7d5c3f9
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-07-17 10:00:21 +0530

    Time: 14 ms (73.54%), Space: 70.1 MB (12.5%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,19 +9,22 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q)return root;
-        return lowestFather(root , p , q );
-    }
-    public TreeNode lowestFather(TreeNode root , TreeNode p , TreeNode q){
-        if(root == null || root == p || root == q){
+        if (root == null || root == p || root == q) {
             return root;
         }
-        TreeNode leftFather = lowestFather(root.left , p , q );
-        TreeNode rightFather = lowestFather(root.right , p , q );
-        if(leftFather == null){
+        return lowest(root, p, q);
+    }
+
+    public TreeNode lowest(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode leftFather = lowest(root.left, p, q);
+        TreeNode rightFather = lowest(root.right, p, q);
+        if (leftFather == null) {
             return rightFather;
         }
-        if(rightFather == null){
+        if (rightFather == null) {
             return leftFather;
         }
         return root;
