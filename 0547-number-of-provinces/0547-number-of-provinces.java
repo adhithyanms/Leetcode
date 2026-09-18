0310853cd6b6fb1062bbcc130fddb0a234e74400
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-09-18 09:05:24 +0530

    Time: 1 ms (85.57%), Space: 47.1 MB (74.37%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,22 +2,23 @@ class Solution {
     public int findCircleNum(int[][] isConnected) {
         int numberOfProvinces = 0;
         int size = isConnected.length;
-        boolean []visited = new boolean[size+1];
-        for(int i = 0 ; i<size;i++){
-            if(!visited[i]){
+        boolean[] visited = new boolean[size + 1];
+        for (int i = 0; i < size; i++) {
+            if (!visited[i]) {
                 numberOfProvinces++;
-                bfs(isConnected , visited , i);
+                bfs(isConnected, visited, i);
             }
         }
         return numberOfProvinces;
     }
-    public void bfs(int[][] isConnected , boolean []visited , int currentIndex){
+
+    public void bfs(int[][] isConnected, boolean[] visited, int currentIndex) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(currentIndex);
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int current = queue.poll();
-            for(int i = 0 ;i<isConnected.length;i++){
-                if(isConnected[current][i]==1 && !visited[i]){
+            for (int i = 0; i < isConnected.length; i++) {
+                if (isConnected[current][i] == 1 && !visited[i]) {
                     queue.offer(i);
                     visited[i] = true;
                 }
