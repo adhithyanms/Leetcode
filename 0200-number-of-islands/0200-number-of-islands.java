4709f482675edf20e5aed4ee4626c59afa84beb7
ADHITHYAN M S <adhithyanshanmugam@gmail.com>
2026-09-18 09:35:41 +0530

    Time: 7 ms (14.55%), Space: 54.2 MB (9.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,42 +1,44 @@
 class Pair {
-    int row;
-    int col;
+    int row, col;
 
     Pair(int row, int col) {
         this.row = row;
         this.col = col;
     }
 }
+
 class Solution {
     public int numIslands(char[][] grid) {
-        int numberIsLands = 0;
+        int numberOfIslands = 0;
         boolean[][] visited = new boolean[grid.length][grid[0].length];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] == '1' && !visited[i][j]){
-                    numberIsLands++;
-                    bfs(i , j , grid , visited);
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    numberOfIslands++;
+                    bfs(i, j, grid, visited);
                 }
             }
         }
-        return numberIsLands;
+        return numberOfIslands;
     }
-    public void bfs(int row , int col , char[][]grid , boolean[][] visited){
+
+    public void bfs(int currentRow, int currentCol, char[][] grid, boolean[][] visited) {
         Queue<Pair> queue = new LinkedList<>();
-        visited[row][col] = true;
-        int rowSize = grid.length;
-        int colSize = grid[0].length;
-        queue.offer(new Pair(row,col));
-        while(queue.size()>0){
+        int row = grid.length;
+        int col = grid[0].length;
+        visited[currentRow][currentCol] = true;
+        queue.offer(new Pair(currentRow, currentCol));
+        while (!queue.isEmpty()) {
             Pair current = queue.poll();
-            int[] directionRow = {-1,0,1,0};
-            int[] directionCol = {0,1,0,-1};
-            for(int i = 0; i<4; i++){
-                int newRow = current.row + directionRow[i];
-                int newCol = current.col + directionCol[i];
-                if(newRow>=0 && newRow<=rowSize-1 && newCol>=0 && newCol<=colSize-1 && grid[newRow][newCol] == '1' && !visited[newRow][newCol]){
+            int[] rowDir = { -1, 0, 1, 0 };
+            int[] colDir = { 0, 1, 0, -1 };
+            for (int i = 0; i < 4; i++) {
+                int newRow = current.row + rowDir[i];
+                int newCol = current.col + colDir[i];
+                if (newRow >= 0 && newCol >= 0 && newRow <= row - 1 && newCol <= col - 1 && grid[newRow][newCol] == '1'
+                        && !visited[newRow][newCol]) {
                     visited[newRow][newCol] = true;
-                    queue.offer(new Pair(newRow,newCol));
+                    queue.offer(new Pair(newRow, newCol));
                 }
             }
         }
